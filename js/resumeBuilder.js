05042cb47ac56a3e0aca58f49b9d97a35b5388d6
chnarjun <chnarjun@gmail.com>
2015-03-24 20:07:19 -0400

    March 23rd changes
    Added name and title to resume

---
+++ js/resumeBuilder.js
@@ -0,0 +1,5 @@
+var formattedName = HTMLheaderName.replace("%data%","Nagarjun Chakilam");
+var formattedRole = HTMLheaderRole.replace("%data%","DSP Quality Engineer");
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
